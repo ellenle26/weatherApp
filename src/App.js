ecc7f0d8c85dd0239e7e4fd4716fe4e55c3b82a5
linhle <lklinh@outlook.com>
2020-09-16 23:30:43 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      weather: null,
+      weatherData: null,
     };
   }
 
-  getWeather = async () => {
+  getWeather = async (lon, lat, city) => {
     let apikey = process.env.REACT_APP_APIKEY;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=${apikey}&units=metric`;
+    let url;
+    if (city) {
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    } else {
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+    }
     let response = await fetch(url);
     let data = await response.json();
+    this.setState({ weatherData: data });
     console.log(data);
-    this.setState({ weather: data });
+  };
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      this.getWeather(pos.coords.longitude, pos.coords.latitude, "");
+    });
   };
 
   componentDidMount = () => {
-    this.getWeather();
+    this.getLocation();
   };
 
   render() {
-    return <div>{this.state.weather && this.state.weather.main.temp}</div>;
+    if (!this.state.weatherData) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <div className="header">
+          <div className="title">
+            <FontAwesomeIcon icon={faCloudSun} color="white" size="3x" /> THE
+            WEATHER
+          </div>
+          <div className="place">
+            <button onClick={() => this.getWeather("", "", "paris")}>
+              Paris
+            </button>
+            <button onClick={() => this.getWeather("", "", "tokyo")}>
+              Tokyo
+            </button>
+            <button onClick={() => this.getWeather("", "", "vancouver")}>
+              Vancouver
+            </button>
+          </div>
+        </div>
+        <div
+          className={`weatherBoard ${this.state.weatherData.name.toLowerCase()}`}
+        >
+          <div className="weatherInfo">
+            <h2>{this.state.weatherData.name}</h2>
+            <p>
+              {this.state.weatherData.main.temp}ºC /
+              {Math.round((this.state.weatherData.main.temp * 9) / 5 + 32)}ºF
+            </p>
+            <div>
+              <img
+                alt=""
+                src={`http://openweathermap.org/img/wn/${this.state.weatherData.weather[0].icon}@2x.png`}
+              />
+            </div>
+            <div>
+              {this.state.weatherData.weather[0].description.toUpperCase()}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
